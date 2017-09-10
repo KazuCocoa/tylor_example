05b5ce5441ca0d1392a1d12dacd0e01c3283aa14
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2017-09-10 08:16:44 +0200

    add an example for taypor

---
+++ README.md
@@ -0,0 +1,9 @@
+Example for [Taylor](https://github.com/yopeso/Taylor)
+
+Run `taylor`
+
+```
+$ taylor -rc ExcessiveClassLength=20 -rc ExcessiveMethodLength=10 -r json:report.json
+```
+
+Result: `report.json`
---
+++ example.swift
@@ -0,0 +1,61 @@
+class Example {
+    let val1: String = "sample"
+
+    func fn1() {
+        "no return value"
+    }
+
+    func fn2TooManyIf() {
+        if(true) {
+            if(true) {
+                if(true) {
+                    if(true) {
+                        if(true) {
+                        }
+                    } else {
+                    }
+                } else {
+                }
+            } else {
+            }
+        } else {
+        }
+        "Cyclomatic Complexity and nexted blocks"
+    }
+
+    func fn3(a: String, _ b: String, _ c: String, _ d: String) {
+        "too many parameters"
+    }
+
+    func fn4() {
+        "no return value"
+    }
+
+    func fn5() {
+        "no return value"
+    }
+
+    func fn6() {
+        "no return value"
+    }
+
+    func fn7() {
+        "no return value"
+    }
+
+    func fn8() {
+        "no return value"
+    }
+
+    func fn9() {
+        "no return value"
+    }
+
+    func fn10() {
+        "no return value"
+    }
+
+    func fn11() {
+        "no return value"
+    }
+}
